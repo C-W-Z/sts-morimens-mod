f1e8ba15ce2060ee3dbdc673149680e8c39aaf11
C-W-Z <chenweizhang3021@gmail.com>
2025-06-19 22:51:50 +0800

    add posse TheGatedAnswer

---
+++ src/main/java/morimensmod/cards/posses/TheGatedAnswer.java
@@ -0,0 +1,49 @@
+package morimensmod.cards.posses;
+
+import static morimensmod.MorimensMod.makeID;
+import static morimensmod.util.Wiz.att;
+import static morimensmod.util.Wiz.getAllPosses;
+import static morimensmod.util.Wiz.p;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+import morimensmod.actions.EasyModalChoiceAction;
+import morimensmod.actions.KeyflareChangeAction;
+import morimensmod.cards.AbstractPosse;
+import morimensmod.characters.AbstractAwakener;
+import morimensmod.misc.PosseType;
+
+public class TheGatedAnswer extends AbstractPosse {
+
+    public final static String ID = makeID(TheGatedAnswer.class.getSimpleName());
+
+    // for register to CardLibrary
+    public TheGatedAnswer() {
+        this(null, PosseType.UNLIMITED);
+    }
+
+    public TheGatedAnswer(AbstractAwakener awaker, PosseType type) {
+        super(ID, awaker, type);
+    }
+
+    @Override
+    public void activate() {
+        att(new KeyflareChangeAction(p(), 200));
+
+        ArrayList<AbstractPosse> posses = getAllPosses();
+
+        for (AbstractPosse p : posses)
+            p.set((AbstractAwakener) p(), PosseType.TMP);
+
+        Collections.shuffle(posses, new Random(AbstractDungeon.miscRng.randomLong()));
+        ArrayList<AbstractCard> choiceCardList = new ArrayList<>(posses.subList(0, Math.min(3, posses.size())));
+        // ArrayList<AbstractCard> choiceCardList = new ArrayList<>(posses);
+
+        att(new EasyModalChoiceAction(choiceCardList));
+    }
+}
