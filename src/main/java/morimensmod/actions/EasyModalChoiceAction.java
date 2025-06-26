b39f80f408f1b692fa7203a001f810303c672490
C-W-Z <chenweizhang3021@gmail.com>
2025-06-26 11:01:18 +0800

    update localization

--- src/main/java/morimensmod/actions/EasyModalChoiceAction.java
+++ src/main/java/morimensmod/actions/EasyModalChoiceAction.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 
 public class EasyModalChoiceAction extends SelectCardsCenteredAction {
 
-    private static final String TEXT = CardCrawlGame.languagePack
-            .getUIString(EasyModalChoiceAction.class.getSimpleName()).TEXT[0];
+    private static final String TEXT[] = CardCrawlGame.languagePack
+            .getUIString(EasyModalChoiceAction.class.getSimpleName()).TEXT;
 
     public EasyModalChoiceAction(ArrayList<AbstractCard> list, int amount, String textforSelect) {
         super(list, amount, textforSelect, (cards) -> {
@@ -20,10 +20,10 @@ public class EasyModalChoiceAction extends SelectCardsCenteredAction {
     }
 
     public EasyModalChoiceAction(ArrayList<AbstractCard> list, int amount) {
-        this(list, amount, TEXT);
+        this(list, amount, TEXT[0] + amount + TEXT[1]);
     }
 
     public EasyModalChoiceAction(ArrayList<AbstractCard> list) {
-        this(list, 1, TEXT);
+        this(list, 1);
     }
 }
