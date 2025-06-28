c2db9a96735db7fce2ab6ee5e657f81f2ad7b2e5
C-W-Z <chenweizhang3021@gmail.com>
2025-06-28 17:38:53 +0800

    update card Showdown

--- src/main/java/morimensmod/cards/chaos/Showdown.java
+++ src/main/java/morimensmod/cards/chaos/Showdown.java
@@ -5,15 +5,18 @@ import static morimensmod.patches.ColorPatch.CardColorPatch.CHAOS_COLOR;
 import static morimensmod.util.Wiz.actB;
 import static morimensmod.util.Wiz.makeInHand;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import basemod.helpers.CardModifierManager;
 import morimensmod.actions.AliemusChangeAction;
 import morimensmod.cardmodifiers.ExhaustModifier;
 import morimensmod.cards.AbstractEasyCard;
+import morimensmod.characters.AbstractAwakener;
 import morimensmod.patches.CustomTags;
 
 public class Showdown extends AbstractEasyCard {
@@ -28,6 +31,7 @@ public class Showdown extends AbstractEasyCard {
         attackCount = baseAttackCount = MAX_ATTACK_COUNT;
         aliemus = baseAliemus = 15;
         magicNumber = baseMagicNumber = 1; // 1 張
+        secondMagic = baseSecondMagic = 0; // 狂氣 per 攻擊
         cardsToPreview = new UnexpectedGain();
         cardsToPreview.modifyCostForCombat(-cardsToPreview.cost);
         cardsToPreview.freeToPlayOnce = true;
@@ -43,6 +47,7 @@ public class Showdown extends AbstractEasyCard {
             actB(() -> {
                 calculateCardDamage(m);
                 dmgTop(m, AttackEffect.NONE);
+                addToTop(new AliemusChangeAction(p, secondMagic));
             });
 
         if (attackCount >= MAX_ATTACK_COUNT) {
@@ -53,5 +58,30 @@ public class Showdown extends AbstractEasyCard {
     @Override
     public void upp() {
         upgradeDamage(3);
+        upgradeSecondMagic(3);
+    }
+
+    @Override
+    public void applyPowers() {
+        int damageAmplify = 100 + baseDamageAmplify + AbstractAwakener.baseDamageAmplify;
+        if (this.hasTag(CardTags.STRIKE))
+            damageAmplify += baseStrikeDamageAmplify;
+        int aliemusAmplify = 100 + baseAliemusAmplify + AbstractAwakener.baseAliemusAmplify;
+
+        AbstractEasyCard tmp = (AbstractEasyCard) CardLibrary.getCopy(cardID, timesUpgraded, misc);
+        baseDamage = MathUtils.ceil(tmp.baseDamage * damageAmplify / 100F);
+        baseAliemus = MathUtils.ceil(tmp.baseAliemus * aliemusAmplify / 100F);
+        baseSecondMagic = MathUtils.ceil(tmp.baseSecondMagic * aliemusAmplify / 100F);
+
+        super.applySuperPower();
+
+        if (damageAmplify != 100)
+            isDamageModified = true;
+        if (aliemusAmplify != 100) {
+            isAliemusModified = true;
+            aliemus = baseAliemus;
+            isSecondMagicModified = true;
+            secondMagic = baseSecondMagic;
+        }
     }
 }
