c6ea5f9ff910f6088a5ae7f24df5effd7d14feb1
C-W-Z <chenweizhang3021@gmail.com>
2025-06-29 13:36:44 +0800

    patch battle reward cards; fix crash bug of card PrecisionSlash

--- src/main/java/morimensmod/cards/chaos/PrecisionSlash.java
+++ src/main/java/morimensmod/cards/chaos/PrecisionSlash.java
@@ -10,10 +10,13 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
 import morimensmod.cards.AbstractEasyCard;
+import morimensmod.characters.AbstractAwakener;
 import morimensmod.patches.CustomTags;
 
 public class PrecisionSlash extends AbstractEasyCard {
@@ -23,6 +26,7 @@ public class PrecisionSlash extends AbstractEasyCard {
     public PrecisionSlash() {
         super(ID, 3, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY, CHAOS_COLOR);
         tags.add(CustomTags.COMMAND);
+        tags.add(CardTags.STRIKE);
         damageType = DamageType.NORMAL; // 必須是normal才能享受力量加成
         damage = baseDamage = 10;
         attackCount = baseAttackCount = 1;
@@ -51,50 +55,78 @@ public class PrecisionSlash extends AbstractEasyCard {
     // 讓卡面顯示的傷害包含格擋加成
     @Override
     public void applyPowers() {
-        int originalStrength = 0;
-        AbstractPower str = p().getPower(StrengthPower.POWER_ID);
-        if (str == null) {
-            str = new StrengthPower(p(), 0);
-            p().powers.add(str);
+        isDamageModified = false;
+
+        int damageAmplify = 100 + baseDamageAmplify + AbstractAwakener.baseDamageAmplify;
+        if (this.hasTag(CardTags.STRIKE))
+            damageAmplify += baseStrikeDamageAmplify;
+        applyedBaseDamageAmplifies(damageAmplify);
+        if (damageAmplify != 100)
+            isDamageModified = true;
+
+        AbstractPlayer player = AbstractDungeon.player;
+        float tmp = baseDamage;
+        for (AbstractRelic r : player.relics) {
+            tmp = r.atDamageModify(tmp, this);
+            if (baseDamage != (int) tmp)
+                isDamageModified = true;
         }
-        else
-            originalStrength = str.amount;
-        // 暫時修改力量 加上格擋
-        str.amount += MathUtils.ceil((float) p().currentBlock / magicNumber);
+        for (AbstractPower p : player.powers)
+            tmp = p.atDamageGive(tmp, damageTypeForTurn, this);
+        tmp = player.stance.atDamageGive(tmp, damageTypeForTurn, this);
 
-        super.applyPowers();
+        // 享受格擋加成
+        tmp += MathUtils.ceil((float) p().currentBlock / magicNumber);
 
-        // 還原力量
-        if (originalStrength == 0)
-            p().powers.remove(p().getPower(StrengthPower.POWER_ID));
-        else
-            str.amount = originalStrength;
-
-        initializeDescription(); // 更新描述中的 !D!
+        if (baseDamage != (int) tmp)
+            isDamageModified = true;
+        for (AbstractPower p : player.powers)
+            tmp = p.atDamageFinalGive(tmp, damageTypeForTurn, this);
+        if (tmp < 0.0F)
+            tmp = 0.0F;
+        if (baseDamage != MathUtils.floor(tmp))
+            isDamageModified = true;
+        damage = MathUtils.floor(tmp);
     }
 
     // 針對單一目標計算最終傷害（例如精英或 Boss）
     @Override
     public void calculateCardDamage(AbstractMonster mo) {
-        int originalStrength = 0;
-        AbstractPower str = p().getPower(StrengthPower.POWER_ID);
-        if (str == null) {
-            str = new StrengthPower(p(), 0);
-            p().powers.add(str);
+        isDamageModified = false;
+
+        int damageAmplify = 100 + baseDamageAmplify + AbstractAwakener.baseDamageAmplify;
+        if (this.hasTag(CardTags.STRIKE))
+            damageAmplify += baseStrikeDamageAmplify;
+        applyedBaseDamageAmplifies(damageAmplify);
+        if (damageAmplify != 100)
+            isDamageModified = true;
+
+        AbstractPlayer player = AbstractDungeon.player;
+        float tmp = baseDamage;
+        for (AbstractRelic r : player.relics) {
+            tmp = r.atDamageModify(tmp, this);
+            if (baseDamage != (int) tmp)
+                isDamageModified = true;
         }
-        else
-            originalStrength = str.amount;
-        // 暫時修改力量 加上格擋
-        str.amount += MathUtils.ceil((float) p().currentBlock / magicNumber);
+        for (AbstractPower p : player.powers)
+            tmp = p.atDamageGive(tmp, damageTypeForTurn, this);
+        tmp = player.stance.atDamageGive(tmp, damageTypeForTurn, this);
 
-        super.calculateCardDamage(mo);
+        // 享受格擋加成
+        tmp += MathUtils.ceil((float) p().currentBlock / magicNumber);
 
-        // 還原力量
-        if (originalStrength == 0)
-            p().powers.remove(p().getPower(StrengthPower.POWER_ID));
-        else
-            str.amount = originalStrength;
-
-        initializeDescription(); // 更新描述中的 !D!
+        if (baseDamage != (int) tmp)
+            isDamageModified = true;
+        for (AbstractPower p : mo.powers)
+            tmp = p.atDamageReceive(tmp, damageTypeForTurn, this);
+        for (AbstractPower p : player.powers)
+            tmp = p.atDamageFinalGive(tmp, damageTypeForTurn, this);
+        for (AbstractPower p : mo.powers)
+            tmp = p.atDamageFinalReceive(tmp, damageTypeForTurn, this);
+        if (tmp < 0.0F)
+            tmp = 0.0F;
+        if (baseDamage != MathUtils.floor(tmp))
+            isDamageModified = true;
+        damage = MathUtils.floor(tmp);
     }
 }
