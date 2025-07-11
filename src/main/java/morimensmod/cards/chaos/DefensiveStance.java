3d2a781dd3d13abd66327bec88f471462d03d75a
C-W-Z <chenweizhang3021@gmail.com>
2025-07-11 14:10:41 +0800

    balance change on card DefensiveStance

--- src/main/java/morimensmod/cards/chaos/DefensiveStance.java
+++ src/main/java/morimensmod/cards/chaos/DefensiveStance.java
@@ -2,7 +2,7 @@ package morimensmod.cards.chaos;
 
 import static morimensmod.MorimensMod.makeID;
 import static morimensmod.patches.ColorPatch.CardColorPatch.CHAOS_COLOR;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.unique.DoubleYourBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import morimensmod.cards.AbstractEasyCard;
@@ -11,16 +11,13 @@ import morimensmod.patches.CustomTags;
 public class DefensiveStance extends AbstractEasyCard {
     public final static String ID = makeID(DefensiveStance.class.getSimpleName());
 
-    private static final int BLOCK_SCALE = 2;
-
     public DefensiveStance() {
         super(ID, 2, CardType.SKILL, CardRarity.RARE, CardTarget.SELF, CHAOS_COLOR);
         tags.add(CustomTags.COMMAND);
-        exhaust = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new GainBlockAction(p, null, p.currentBlock * (BLOCK_SCALE - 1)));
+        addToBot(new DoubleYourBlockAction(p));
     }
 
     @Override
