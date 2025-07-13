5e018e0cb763aa0ab2f443f555fc52a75bcf9451
C-W-Z <chenweizhang3021@gmail.com>
2025-07-13 11:39:56 +0800

    Encapsulate monsters

---
+++ src/main/java/morimensmod/monsters/AbstractMorimensMonster.java
@@ -0,0 +1,71 @@
+package morimensmod.monsters;
+
+import java.util.ArrayList;
+
+import com.megacrit.cardcrawl.actions.common.RollMoveAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import basemod.abstracts.CustomMonster;
+import basemod.animations.AbstractAnimation;
+import morimensmod.misc.Animator;
+import morimensmod.powers.MadnessPower;
+import morimensmod.powers.TmpMadnessPower;
+import morimensmod.util.ModSettings;
+
+import static morimensmod.util.Wiz.getPowerAmount;
+
+public abstract class AbstractMorimensMonster extends CustomMonster {
+
+    protected ArrayList<Integer> attackCount = new ArrayList<>();
+    protected int turn = 0;
+
+    public AbstractMorimensMonster(String name, String id, int maxHealth, float hb_w, float hb_h, float x, float y) {
+        // 名字
+        // ID
+        // 最大生命值，由於之後還會設定可以隨意填寫
+        // hitbox偏移量 - x方向
+        // hitbox偏移量 - y方向
+        // hitbox大小 - x方向（會影響血條寬度）
+        // hitbox大小 - y方向
+        // 圖片
+        // 怪物位置（x,y）
+        super(name, id, maxHealth, 0F, 0.0F, hb_w, hb_h, null, x, y);
+        this.animation = getAnimation();
+    }
+
+    protected abstract AbstractAnimation getAnimation();
+
+    protected int getAttackCount(int move) {
+        return attackCount.get(move)
+                + getPowerAmount(this, MadnessPower.POWER_ID)
+                + getPowerAmount(this, TmpMadnessPower.POWER_ID);
+    }
+
+    // 戰鬥開始
+    @Override
+    public void usePreBattleAction() {
+        turn = 0;
+    }
+
+    // 執行動作
+    @Override
+    public void takeTurn() {
+        turn++;
+        // 要加一個rollmove的action，重roll怪物的意圖
+        addToBot(new RollMoveAction(this));
+    }
+
+    @Override
+    public void changeState(String stateName) {
+        if (this.animation instanceof Animator)
+            ((Animator) this.animation).setAnimation(stateName);
+    }
+
+    @Override
+    public void damage(DamageInfo info) {
+        super.damage(info);
+        if (!(this.animation instanceof Animator))
+            return;
+        if (info.owner != null && info.type != DamageInfo.DamageType.THORNS && info.output > 0)
+            ((Animator) this.animation).setAnimation(ModSettings.MONSTER_HIT_ANIM);
+    }
+}
--- src/main/java/morimensmod/monsters/Hardhitter.java
+++ src/main/java/morimensmod/monsters/Hardhitter.java
@@ -3,17 +3,13 @@ package morimensmod.monsters;
 import static morimensmod.MorimensMod.makeID;
 import static morimensmod.MorimensMod.makeMonsterPath;
 import static morimensmod.util.General.removeModID;
-import static morimensmod.util.Wiz.getPowerAmount;
 import static morimensmod.util.Wiz.p;
-
-import java.util.ArrayList;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.ChangeStateAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.common.RollMoveAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -21,38 +17,22 @@ import com.megacrit.cardcrawl.localization.MonsterStrings;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.powers.WeakPower;
 
-import basemod.abstracts.CustomMonster;
+import basemod.animations.AbstractAnimation;
 import morimensmod.actions.NewWaitAction;
 import morimensmod.misc.Animator;
-import morimensmod.powers.MadnessPower;
-import morimensmod.powers.TmpMadnessPower;
 import morimensmod.util.ModSettings;
 
-public class Hardhitter extends CustomMonster {
+public class Hardhitter extends AbstractMorimensMonster {
 
     public static final String ID = makeID(Hardhitter.class.getSimpleName());
     private static final MonsterStrings monsterStrings = CardCrawlGame.languagePack.getMonsterStrings(ID);
     public static final String NAME = monsterStrings.NAME;
 
-    protected ArrayList<Integer> attackCount = new ArrayList<>();
-
     private static final float xOffset = -32;
     private static final float yOffset = 0;
 
-    private int turn = 0;
-
     public Hardhitter(float x, float y) {
-        // 参数的作用分别是：
-        // 名字
-        // ID
-        // 最大生命值，由于在之后还会设置可以随意填写
-        // hitbox偏移量 - x方向
-        // hitbox偏移量 - y方向
-        // hitbox大小 - x方向（会影响血条宽度）
-        // hitbox大小 - y方向
-        // 图片
-        // 怪物位置（x,y）
-        super(NAME, ID, 50, 0F, 0F, 241F, 268.F, null, x, y);
+        super(NAME, ID, 50, 250F, 270F, x, y);
 
         // 如果你要做进阶改变血量和伤害意图等，这样写
         if (AbstractDungeon.ascensionLevel >= 7)
@@ -76,7 +56,10 @@ public class Hardhitter extends CustomMonster {
         attackCount.add(2);
         damage.add(new DamageInfo(this, slashDmg));
         attackCount.add(1);
+    }
 
+    @Override
+    protected AbstractAnimation getAnimation() {
         // xOffset是Idle_1和另一張圖置中疊在一起之後，另一張要水平移動多少距離才會和Idle_1水平位置重合
         // yOffset是Idle_1和另一張貼圖齊底部疊在一起後，另一張要垂直移動多少才會和Idle_1高度相同
         Animator animator = new Animator();
@@ -98,20 +81,15 @@ public class Hardhitter extends CustomMonster {
                 makeMonsterPath(removeModID(ID) + "/" + ModSettings.MONSTER_SKILL1_ANIM + ".png"),
                 6, 7, 0, false, xOffset + 29F, yOffset);
         animator.setDefaultAnim(ModSettings.MONSTER_IDLE_ANIM);
-        this.animation = animator;
-    }
-
-    protected int getAttackCount(int move) {
-        return attackCount.get(move) + getPowerAmount(this, MadnessPower.POWER_ID) + getPowerAmount(this, TmpMadnessPower.POWER_ID);
+        return animator;
     }
 
     // 战斗开始时
-    @Override
-    public void usePreBattleAction() {
-        turn = 0;
-        // super.usePreBattleAction();
-        addToBot(new ApplyPowerAction(this, this, new MadnessPower(this, 1)));
-    }
+    // @Override
+    // public void usePreBattleAction() {
+    //     super.usePreBattleAction();
+    //     addToBot(new ApplyPowerAction(this, this, new MadnessPower(this, 1)));
+    // }
 
     // 当怪物roll意图的时候，这里设置其意图。num是一个0~99的随机数。
     @Override
@@ -156,20 +134,6 @@ public class Hardhitter extends CustomMonster {
                 break;
         }
 
-        turn++;
-
-        // 要加一个rollmove的action，重roll怪物的意图
-        addToBot(new RollMoveAction(this));
-    }
-
-    @Override
-    public void changeState(String stateName) {
-        ((Animator) this.animation).setAnimation(stateName);
-    }
-
-    public void damage(DamageInfo info) {
-        super.damage(info);
-        if (info.owner != null && info.type != DamageInfo.DamageType.THORNS && info.output > 0)
-            ((Animator) this.animation).setAnimation(ModSettings.MONSTER_HIT_ANIM);
+        super.takeTurn();
     }
 }
