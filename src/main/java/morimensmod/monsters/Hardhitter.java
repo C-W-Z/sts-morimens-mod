c3f81f5c9dae4988534fe6d3fb3915f042dd6fa9
C-W-Z <chenweizhang3021@gmail.com>
2025-07-13 11:09:05 +0800

    add MadnessPower; update monster attack count

--- src/main/java/morimensmod/monsters/Hardhitter.java
+++ src/main/java/morimensmod/monsters/Hardhitter.java
@@ -3,7 +3,10 @@ package morimensmod.monsters;
 import static morimensmod.MorimensMod.makeID;
 import static morimensmod.MorimensMod.makeMonsterPath;
 import static morimensmod.util.General.removeModID;
+import static morimensmod.util.Wiz.getPowerAmount;
 import static morimensmod.util.Wiz.p;
+
+import java.util.ArrayList;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
@@ -21,6 +24,8 @@ import com.megacrit.cardcrawl.powers.WeakPower;
 import basemod.abstracts.CustomMonster;
 import morimensmod.actions.NewWaitAction;
 import morimensmod.misc.Animator;
+import morimensmod.powers.MadnessPower;
+import morimensmod.powers.TmpMadnessPower;
 import morimensmod.util.ModSettings;
 
 public class Hardhitter extends CustomMonster {
@@ -28,6 +33,8 @@ public class Hardhitter extends CustomMonster {
     public static final String ID = makeID(Hardhitter.class.getSimpleName());
     private static final MonsterStrings monsterStrings = CardCrawlGame.languagePack.getMonsterStrings(ID);
     public static final String NAME = monsterStrings.NAME;
+
+    protected ArrayList<Integer> attackCount = new ArrayList<>();
 
     private static final float xOffset = -32;
     private static final float yOffset = 0;
@@ -64,8 +71,11 @@ public class Hardhitter extends CustomMonster {
             slashDmg = 10;
         }
         damage.add(new DamageInfo(this, 0));
+        attackCount.add(0);
         damage.add(new DamageInfo(this, multiDmg));
+        attackCount.add(2);
         damage.add(new DamageInfo(this, slashDmg));
+        attackCount.add(1);
 
         // xOffset是Idle_1和另一張圖置中疊在一起之後，另一張要水平移動多少距離才會和Idle_1水平位置重合
         // yOffset是Idle_1和另一張貼圖齊底部疊在一起後，另一張要垂直移動多少才會和Idle_1高度相同
@@ -91,23 +101,33 @@ public class Hardhitter extends CustomMonster {
         this.animation = animator;
     }
 
+    protected int getAttackCount(int move) {
+        return attackCount.get(move) + getPowerAmount(this, MadnessPower.POWER_ID) + getPowerAmount(this, TmpMadnessPower.POWER_ID);
+    }
+
     // 战斗开始时
     @Override
     public void usePreBattleAction() {
         turn = 0;
         // super.usePreBattleAction();
-        // addToBot(new ApplyPowerAction(this, this, new RitualPower(this, 5, false)));
+        addToBot(new ApplyPowerAction(this, this, new MadnessPower(this, 1)));
     }
 
     // 当怪物roll意图的时候，这里设置其意图。num是一个0~99的随机数。
     @Override
     public void getMove(int num) {
-        if (turn % 3 == 0)
-            setMove((byte) 0, Intent.DEFEND_BUFF, 0);
-        else if (turn % 3 == 1)
-            setMove((byte) 1, Intent.ATTACK, damage.get(1).base, 2, true);
-        else if (turn % 3 == 2)
-            setMove((byte) 2, Intent.ATTACK_DEBUFF, damage.get(2).base);
+        switch (turn % 3) {
+            case 0:
+                setMove((byte) 0, Intent.DEFEND_BUFF, 0);
+                break;
+            case 1:
+                setMove((byte) 1, Intent.ATTACK, damage.get(1).base, getAttackCount(1), true);
+                break;
+            case 2:
+                setMove((byte) 2, Intent.ATTACK_DEBUFF, damage.get(2).base, getAttackCount(2), getAttackCount(2) != 1);
+            default:
+                break;
+        }
     }
 
     // 执行动作
@@ -124,13 +144,14 @@ public class Hardhitter extends CustomMonster {
             case 1:
                 addToBot(new ChangeStateAction(this, ModSettings.MONSTER_ATTACK_ANIM));
                 addToBot(new NewWaitAction(0.5F));
-                addToBot(new DamageAction(p(), damage.get(1), AttackEffect.BLUNT_LIGHT));
-                addToBot(new DamageAction(p(), damage.get(1), AttackEffect.BLUNT_HEAVY));
+                for (int i = 0; i < getAttackCount(1); i++)
+                    addToBot(new DamageAction(p(), damage.get(1), AttackEffect.BLUNT_LIGHT));
                 break;
             case 2:
                 addToBot(new ChangeStateAction(this, ModSettings.MONSTER_ATTACK_ANIM));
                 addToBot(new NewWaitAction(0.5F));
-                addToBot(new DamageAction(p(), damage.get(2), AttackEffect.BLUNT_HEAVY));
+                for (int i = 0; i < getAttackCount(2); i++)
+                    addToBot(new DamageAction(p(), damage.get(2), AttackEffect.BLUNT_HEAVY));
                 addToBot(new ApplyPowerAction(p(), this, new WeakPower(p(), 1, true)));
                 break;
         }
