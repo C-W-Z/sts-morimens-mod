5f1fe87a3a5af2151ac47fb41803cc67aa948dcf
C-W-Z <chenweizhang3021@gmail.com>
2025-07-13 09:53:04 +0800

    add Ramona Animation; remove renderCorpse

--- src/main/java/morimensmod/characters/Ramona.java
+++ src/main/java/morimensmod/characters/Ramona.java
@@ -5,14 +5,16 @@ import morimensmod.cards.chaos.Defend;
 import morimensmod.cards.chaos.QueensSword;
 import morimensmod.cards.chaos.Strike;
 import morimensmod.exalts.MundusDecree;
-import morimensmod.misc.SpriteSheetAnimation;
+import morimensmod.misc.Animator;
 import morimensmod.relics.ChaosRelic;
 import morimensmod.relics.RamonaRelic;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.helpers.CardLibrary;
@@ -20,11 +22,13 @@ import com.megacrit.cardcrawl.localization.CharacterStrings;
 import com.megacrit.cardcrawl.screens.CharSelectInfo;
 
 import basemod.BaseMod;
+import basemod.animations.AbstractAnimation;
 
 import static morimensmod.MorimensMod.*;
 import static morimensmod.patches.ColorPatch.CardColorPatch.BUFF_COLOR;
 import static morimensmod.patches.ColorPatch.CardColorPatch.CHAOS_COLOR;
 import static morimensmod.patches.ColorPatch.CardColorPatch.WHEEL_OF_DESTINY_COLOR;
+import static morimensmod.util.General.removeModID;
 
 import java.util.ArrayList;
 
@@ -38,15 +42,55 @@ public class Ramona extends AbstractAwakener {
     private static final String CHARSELECT_BUTTON = makeCharacterPath("Ramona/button.png");
     private static final String CHARSELECT_PORTRAIT = makeCharacterPath("Ramona/charBG.png");
 
+    private static final float xOffset = -22;
+    private static final float yOffset = -10;
+
     public Ramona() {
         super(NAMES[0], Enums.RAMONA, "Ramona/main.png", "Ramona/main.png");
-        anim = new SpriteSheetAnimation(makeCharacterPath("Ramona/Idle_1.png"),
-                6, 17, 1, true, -22, -10);
         exalt = new MundusDecree();
         baseAliemusRegen = 0;
         baseKeyflareRegen = 60;
         deathResistance = 100;
         baseRealmMastery = 50;
+
+        Animator animator = new Animator();
+        animator.addAnimation(
+                "Idle_1",
+                makeCharacterPath(removeModID(ID) + "/Idle_1.png"),
+                6, 17, 1, true, xOffset, yOffset);
+        animator.addAnimation(
+                "Hit",
+                makeCharacterPath(removeModID(ID) + "/Hit.png"),
+                4, 5, 0, false, xOffset - 17.5F, yOffset);
+        animator.addAnimation(
+                "Defence",
+                makeCharacterPath(removeModID(ID) + "/Defence.png"),
+                4, 8, 1, false, xOffset + 97.5F, yOffset);
+        animator.addAnimation(
+                "Attack",
+                makeCharacterPath(removeModID(ID) + "/Attack.png"),
+                9, 3, 2, false, xOffset + 202F, yOffset);
+        animator.setDefaultAnim("Idle_1");
+        this.animation = animator;
+
+        // in AbstractPlayer.render(), there's
+        // if (this.atlas != null && !this.renderCorpse) {
+        //     this.renderPlayerImage(sb);
+        if (animation.type() != AbstractAnimation.Type.NONE)
+            this.atlas = new TextureAtlas();
+    }
+
+    @Override
+    public void damage(DamageInfo info) {
+        int hp = currentHealth;
+        int block = currentBlock;
+        super.damage(info);
+        if (info.owner != null && info.type != DamageInfo.DamageType.THORNS && info.output > 0) {
+            if (hp == currentHealth && block > 0 && currentBlock >= 0)
+                ((Animator) this.animation).setAnimation("Defence");
+            else
+                ((Animator) this.animation).setAnimation("Hit");
+        }
     }
 
     public static void register() {
