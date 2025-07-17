c286e74e403d546fde2c53571c88d4764ea8aa61
C-W-Z <chenweizhang3021@gmail.com>
2025-07-17 13:48:22 +0800

    gain a random common relic on picked up ChaosRelic

--- src/main/java/morimensmod/relics/ChaosRelic.java
+++ src/main/java/morimensmod/relics/ChaosRelic.java
@@ -4,7 +4,11 @@ import static morimensmod.MorimensMod.makeID;
 import static morimensmod.util.Wiz.p;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 
 import morimensmod.actions.AliemusChangeAction;
 import morimensmod.actions.KeyflareChangeAction;
@@ -16,6 +20,7 @@ import morimensmod.misc.PosseType;
 public class ChaosRelic extends AbstractEasyRelic implements OnAfterPosse {
     public static final String ID = makeID(ChaosRelic.class.getSimpleName());
 
+    private static final int COMMON_RELIC = 1;
     private static final int KEYFLARE = 250;
     private static final int REALM_MASTERY = 10;
     private static final int ALIEMUS = 1;
@@ -44,11 +49,40 @@ public class ChaosRelic extends AbstractEasyRelic implements OnAfterPosse {
     }
 
     @Override
+    public void onEquip() {
+        AbstractDungeon.topLevelEffects.add(new AbstractGameEffect() {
+            private boolean triggered = false;
+
+            @Override
+            public void update() {
+                if (!triggered && AbstractDungeon.player != null && AbstractDungeon.getCurrRoom() != null) {
+                    triggered = true;
+                    // TODO: 未來可以做一個圖示是問號的遺物，效果是變成隨機遺物，也就是移除自身並獲得另一個隨機遺物
+                    // AbstractDungeon.player.loseRelic(relicId);
+
+                    // 隨機選一個普通遺物
+                    AbstractRelic newRelic = AbstractDungeon.returnRandomScreenlessRelic(RelicTier.COMMON);
+                    // 實際給予
+                    AbstractDungeon.getCurrRoom().spawnRelicAndObtain(
+                            Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F, newRelic);
+                }
+                this.isDone = triggered;
+            }
+
+            @Override
+            public void render(SpriteBatch sb) {}
+
+            @Override
+            public void dispose() {}
+        });
+    }
+
+    @Override
     public String getUpdatedDescription() {
         if (!(p() instanceof AbstractAwakener))
-            return String.format(DESCRIPTIONS[0], KEYFLARE, REALM_MASTERY, ALIEMUS);
+            return String.format(DESCRIPTIONS[0], COMMON_RELIC, KEYFLARE, REALM_MASTERY, ALIEMUS);
         AbstractAwakener awaker = (AbstractAwakener) p();
-        return String.format(DESCRIPTIONS[0], KEYFLARE, REALM_MASTERY, ALIEMUS)
+        return String.format(DESCRIPTIONS[0], COMMON_RELIC, KEYFLARE, REALM_MASTERY, ALIEMUS)
                 + String.format(DESCRIPTIONS[1], awaker.getRealmMastry(), awaker.getRealmMastry() / REALM_MASTERY);
     }
 
