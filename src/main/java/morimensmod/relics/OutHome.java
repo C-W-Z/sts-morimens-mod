ae02efaaaabf1995455e92fa6fb3072c4ba16b08
C-W-Z <chenweizhang3021@gmail.com>
2025-07-18 12:39:15 +0800

    add relic OurHome & update localization

---
+++ src/main/java/morimensmod/relics/OutHome.java
@@ -0,0 +1,43 @@
+package morimensmod.relics;
+
+import static morimensmod.MorimensMod.makeID;
+import static morimensmod.util.Wiz.*;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.LoseStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+public class OutHome extends AbstractEasyRelic {
+    public static final String ID = makeID(OutHome.class.getSimpleName());
+
+    private static final int BLOCK_PER_ATTACK = 1;
+    private static final int TMP_STR_PER_HEAL = 2;
+
+    public OutHome() {
+        super(ID, RelicTier.RARE, LandingSound.HEAVY);
+    }
+
+    @Override
+    public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
+        if (info.type == DamageType.THORNS)
+            return;
+        flash();
+        addToBot(new GainBlockAction(p(), BLOCK_PER_ATTACK));
+    }
+
+    @Override
+    public int onPlayerHeal(int healAmount) {
+        flash();
+        applyToSelf(new StrengthPower(p(), TMP_STR_PER_HEAL));
+        applyToSelf(new LoseStrengthPower(p(), TMP_STR_PER_HEAL));
+        return super.onPlayerHeal(healAmount);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return String.format(DESCRIPTIONS[0], BLOCK_PER_ATTACK, TMP_STR_PER_HEAL);
+    }
+}
