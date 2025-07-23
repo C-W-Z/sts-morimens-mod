5ff1db5d04ea8fda5000d048eefaaafdfc16636a
C-W-Z <chenweizhang3021@gmail.com>
2025-07-23 21:37:21 +0800

    v0.7.0

--- src/main/java/morimensmod/patches/MapNodeToolTipPatch.java
+++ src/main/java/morimensmod/patches/MapNodeToolTipPatch.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.FontHelper;
