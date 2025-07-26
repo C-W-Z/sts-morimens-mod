3e7ecaafdfc2c6effddac1f3b97393a2903592c5
C-W-Z <chenweizhang3021@gmail.com>
2025-07-26 16:30:17 +0800

    No amplify on BUFF & SYMPTOM cards; add SymptomMadness

--- src/main/java/morimensmod/cards/buffs/AbstractBuffCard.java
+++ src/main/java/morimensmod/cards/buffs/AbstractBuffCard.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.SpawnModificationCard;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import morimensmod.cards.AbstractEasyCard;
 import morimensmod.patches.enums.CustomTags;
@@ -19,6 +20,16 @@ public abstract class AbstractBuffCard extends AbstractEasyCard implements Spawn
 
     public AbstractBuffCard(final String cardID, final int cost, final CardRarity rarity, final CardTarget target) {
         this(cardID, cardID, cost, rarity, target);
+    }
+
+    @Override
+    public void applyPowers() {
+        super.applySuperPower();
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        super.calculateSuperCardDamage(mo);
     }
 
     @Override
---
+++ src/main/java/morimensmod/cards/symptoms/SymptomMadness.java
@@ -0,0 +1,27 @@
+package morimensmod.cards.symptoms;
+
+import static morimensmod.MorimensMod.makeID;
+
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import morimensmod.actions.AliemusChangeAction;
+import morimensmod.patches.enums.CustomTags;
+
+public class SymptomMadness extends AbstractSymptomCard {
+    public final static String ID = makeID(SymptomMadness.class.getSimpleName());
+
+    public SymptomMadness() {
+        super(ID, 0, CardTarget.SELF);
+        tags.add(CustomTags.UNREMOVABLE_IN_SHOP);
+        magicNumber = baseMagicNumber = 15; // 失去生命
+        aliemus = baseAliemus = 10;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new LoseHPAction(p, null, magicNumber));
+        addToBot(new AliemusChangeAction(p, aliemus));
+    }
+}
