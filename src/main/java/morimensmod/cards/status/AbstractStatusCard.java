07cf858858452422b05260d3069de2f2376a9aae
C-W-Z <chenweizhang3021@gmail.com>
2025-07-26 21:00:14 +0800

    fix flavor color for State Cards

--- src/main/java/morimensmod/cards/status/AbstractStatusCard.java
+++ src/main/java/morimensmod/cards/status/AbstractStatusCard.java
@@ -2,7 +2,10 @@ package morimensmod.cards.status;
 
 import static morimensmod.patches.enums.ColorPatch.CardColorPatch.STATUS_COLOR;
 
+import com.evacipated.cardcrawl.mod.stslib.patches.FlavorText.AbstractCardFlavorFields;
+
 import morimensmod.cards.AbstractEasyCard;
+import morimensmod.config.ModSettings;
 import morimensmod.patches.enums.CustomTags;
 
 public abstract class AbstractStatusCard extends AbstractEasyCard {
@@ -10,6 +13,8 @@ public abstract class AbstractStatusCard extends AbstractEasyCard {
     public AbstractStatusCard(final String cardID, final int cost, final CardRarity rarity, final CardTarget target) {
         super(cardID, cost, CardType.STATUS, rarity, target, STATUS_COLOR);
         tags.add(CustomTags.STATUS);
+        AbstractCardFlavorFields.boxColor.set(this, ModSettings.STATUS_CARD_FLAVOR_BOX_COLOR);
+        AbstractCardFlavorFields.textColor.set(this, ModSettings.STATUS_CARD_FLAVOR_TEXT_COLOR);
     }
 
     @Override
