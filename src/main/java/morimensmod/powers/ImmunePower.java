1cde79f5b20f8b8d6b541d92de91fe3669d80d37
C-W-Z <chenweizhang3021@gmail.com>
2025-07-27 22:48:12 +0800

    add ImmunePower

---
+++ src/main/java/morimensmod/powers/ImmunePower.java
@@ -0,0 +1,39 @@
+package morimensmod.powers;
+
+import static morimensmod.MorimensMod.makeID;
+
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+
+import morimensmod.interfaces.OnBeforeDamaged;
+
+public class ImmunePower extends AbstractEasyPower implements OnBeforeDamaged {
+
+    public final static String POWER_ID = makeID(ImmunePower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    private static final String NAME = powerStrings.NAME;
+    private static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public ImmunePower(AbstractCreature owner) {
+        super(POWER_ID, NAME, PowerType.BUFF, false, owner, -1);
+        priority = 1;
+    }
+
+    @Override
+    public int onBeforeDamaged(DamageInfo info, int damageAmount) {
+        return 0;
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+}
