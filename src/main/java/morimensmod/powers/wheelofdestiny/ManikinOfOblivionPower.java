223619b8a77cbe1d9b038bd553cb1eed336e72e6
C-W-Z <chenweizhang3021@gmail.com>
2025-08-05 16:09:42 +0800

    add Frenzy

--- src/main/java/morimensmod/powers/wheelofdestiny/ManikinOfOblivionPower.java
+++ src/main/java/morimensmod/powers/wheelofdestiny/ManikinOfOblivionPower.java
@@ -28,7 +28,6 @@ public class ManikinOfOblivionPower extends AbstractEasyPower implements OnAfter
 
     public ManikinOfOblivionPower(AbstractCreature owner, int percent) {
         super(POWER_ID, NAME, PowerType.BUFF, false, owner, percent);
-        updateDescription();
     }
 
     @Override
