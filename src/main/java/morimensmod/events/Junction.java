da90622b2c46242939583c5f1a94ca29aa487330
C-W-Z <chenweizhang3021@gmail.com>
2025-08-05 23:05:28 +0800

    add bad option in Juction Event after A15

--- src/main/java/morimensmod/events/Junction.java
+++ src/main/java/morimensmod/events/Junction.java
@@ -2,9 +2,13 @@ package morimensmod.events;
 
 import static morimensmod.MorimensMod.makeEventPath;
 import static morimensmod.MorimensMod.makeID;
+import static morimensmod.util.CardLib.getRandomSymptoms;
 import static morimensmod.util.General.normalizeWhitespace;
 import static morimensmod.util.General.removeModID;
 import static morimensmod.util.Wiz.p;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -18,6 +22,7 @@ import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
 
 import morimensmod.cards.buffs.Insight;
 import morimensmod.characters.AbstractAwakener;
+import morimensmod.config.ModSettings;
 
 public class Junction extends AbstractImageEvent {
 
@@ -35,6 +40,8 @@ public class Junction extends AbstractImageEvent {
 
     private int healAmt;
     private AbstractCard obtainCard;
+    private static final int N_SYMPTOM = 1;
+    private ArrayList<AbstractCard> obtainSymptoms;
 
     public Junction() {
         super(title, DESCRIPTIONS[0], makeEventPath(removeModID(ID) + ".png"));
@@ -60,11 +67,19 @@ public class Junction extends AbstractImageEvent {
         if (canRouse) {
             AbstractAwakener awaker = (AbstractAwakener) p();
             obtainCard = awaker.rouseCard.makeCopy();
-            this.imageEventText.setDialogOption(
-                    String.format(OPTIONS[1], "#y" + normalizeWhitespace(obtainCard.name).replaceAll(" ", " #y")),
-                    obtainCard);
         } else {
             obtainCard = new Insight();
+        }
+
+        if (AbstractDungeon.ascensionLevel >= ModSettings.ASCENSION_LVL.BAD_EVENT) {
+            obtainSymptoms = getRandomSymptoms(N_SYMPTOM);
+            this.imageEventText.setDialogOption(
+                    String.format(
+                            OPTIONS[2],
+                            "#y" + normalizeWhitespace(obtainCard.name).replaceAll(" ", " #y"),
+                            N_SYMPTOM),
+                    obtainCard);
+        } else {
             this.imageEventText.setDialogOption(
                     String.format(OPTIONS[1], "#y" + normalizeWhitespace(obtainCard.name).replaceAll(" ", " #y")),
                     obtainCard);
@@ -79,20 +94,31 @@ public class Junction extends AbstractImageEvent {
                 switch (buttonPressed) {
                     case 0:
                         p().heal(healAmt, true);
-                        AbstractEvent.logMetricHeal(title, OPTIONS[3], healAmt);
+                        AbstractEvent.logMetricHeal(title, OPTIONS[4], healAmt);
                         break;
                     case 1:
                         // 究極噁心玩意，到底哪個大聰明把獲得卡牌跟特效綁定在一起的
                         AbstractDungeon.effectList.add(
                                 new ShowCardAndObtainEffect(obtainCard, Settings.WIDTH / 2F, Settings.HEIGHT / 2F));
-                        AbstractEvent.logMetricObtainCard(title, OPTIONS[4], obtainCard);
+                        if (AbstractDungeon.ascensionLevel >= ModSettings.ASCENSION_LVL.BAD_EVENT) {
+                            List<String> tempList = new ArrayList<>();
+                            tempList.add(obtainCard.cardID);
+                            for (AbstractCard s : obtainSymptoms) {
+                                AbstractDungeon.effectList.add(
+                                        new ShowCardAndObtainEffect(s, Settings.WIDTH / 2F, Settings.HEIGHT / 2F));
+                                tempList.add(s.cardID);
+                            }
+                            AbstractEvent.logMetricObtainCards(title, OPTIONS[6], tempList);
+                        } else {
+                            AbstractEvent.logMetricObtainCard(title, OPTIONS[5], obtainCard);
+                        }
                         break;
                     default:
                         logMetricIgnored(title);
                         break;
                 }
                 this.imageEventText.clearAllDialogs();
-                this.imageEventText.setDialogOption(OPTIONS[2]);
+                this.imageEventText.setDialogOption(OPTIONS[3]);
                 this.screen = CurScreen.RESULT;
                 break;
             default:
